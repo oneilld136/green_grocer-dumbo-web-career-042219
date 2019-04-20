4b630bdf8a8da1f7d6724129ed8e7227b5ccb6eb
oneilld136 <oneilld136@gmail.com>
2019-04-20 18:44:57 +0000

    Automatically backed up by Learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -55,3 +55,56 @@ coupons.each do |coupon|
 end
 
 puts "Your total is #{checkout(cart: cart, coupons: coupons)}"
+
+
+def consolidate_cart(cart)
+  organized_cart = {}
+  count = 0
+  cart.each do |element|
+    element.each do |fruit, hash|
+      organized_cart[fruit] ||= hash
+      organized_cart[fruit][:count] ||= 0
+      organized_cart[fruit][:count] += 1
+    end
+  end
+  return organized_cart
+end
+
+def apply_coupons(cart, coupons)
+  coupons.each do |coupon_hash|
+    fruit_name = coupon_hash[:item]
+    new_coupon_hash = {
+      :price => coupon_hash[:cost],
+      :clearance => "true",
+      :count => coupon_hash[:num]
+    }
+    
+     if cart.key?(fruit_name)
+      new_coupon_hash[:clearance] = cart[fruit_name][:clearance]
+      if cart[fruit_name][:count]>= new_coupon_hash[:count]
+        new_coupon_hash[:count] = (cart[fruit_name][:count]/new_coupon_hash[:count]).floor
+        cart[fruit_name][:count] = (coupon_hash[:num])%(cart[fruit_name][:count])
+      end
+      cart[fruit_name + " W/COUPON"] = new_coupon_hash 
+    end
+    end
+  return cart
+end
+
+test_cart = {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 4},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+}
+
+test_coupon = 
+[
+		{:item => "AVOCADO", :num => 2, :cost => 5.00},
+		{:item => "BEER", :num => 2, :cost => 20.00},
+		{:item => "CHEESE", :num => 3, :cost => 15.00}
+	]
+	
+result = 
+{
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 2},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+}
